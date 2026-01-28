e95cf6e0027a1eb275fd7e76609efbb5737543cf
krishnalavanya <krishnalavanyakasireddi@gmail.com>
2026-01-28 15:14:15 -0500

    variables

--- 02-variables-prac.sh
+++ 02-variables-prac.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-PERSON1=Siva
-PERSON2=Durga
+PERSON1=$1
+PERSON2=$2
 echo "$PERSON1:: Hi $PERSON2 how are you?"
 echo "$PERSON2:: Hi $PERSON1, I am good, how about you?"
 echo "$PERSON1:: I am doing good, what are you doing?"
-echo "$PERSON2:: I am having lunch, what about you?"
+echo "$PERSON2:: I am having lunch, what about you?" 
 
 
