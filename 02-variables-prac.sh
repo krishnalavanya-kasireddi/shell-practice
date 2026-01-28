cc8e4c7d26b2bce6c4cd83fcc0024208fb30e9f1
krishnalavanya <krishnalavanyakasireddi@gmail.com>
2026-01-28 14:24:26 -0500

    variables

---
+++ 02-variables-prac.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo "Tom:: Hi Jerry, how are you?"
+echo "Jerry:: Hi Tom, I am good, how about you?"
+echo "Tomm:: I am doing good, what are you doing?"
+echo "Jerry:: I am having lunch, what about you?"
+
+
