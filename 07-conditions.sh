37cd5809c23be4e8ba710fdc000ed40099708cf7
krishnalavanya <krishnalavanyakasireddi@gmail.com>
2026-01-29 17:10:23 -0500

    variables

--- 07-conditions.sh
+++ 07-conditions.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
 echo "given number is"
 read num
-if [ $num > 30 ];
-
-then
-
-echo "given number is greater than 30"
-elif [ $num -eq 30 ];
-then
-echo "given number is equal to 30"
+if [ $num > 30 ]; then
+   echo "given number is greater than 30"
+elif [ $num -eq 30 ]; then
+     echo "given number is equal to 30"
 else
-echo " given number is less than 30"
+     echo " given number is less than 30"
 fi
