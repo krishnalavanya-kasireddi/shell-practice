54a3ae952c07d6a2162179e9d47f660a26dbce2e
krishnalavanya <krishnalavanyakasireddi@gmail.com>
2026-01-29 14:21:31 -0500

    variables

--- 04-command.sh
+++ 04-command.sh
@@ -1,3 +1,14 @@
 #!/bin/bash
-directory=$(pwd)
-echo "show working directory $directory"
+
+
+start_time=$(date +%s)
+
+echo "script executed time at $start_time"
+
+sleep 10
+
+end_time=$(date +%s)
+
+total_time=$(($end_time-$start_time))
+
+echo "script executed time $total_time seconds"
