97ed96079a31bc551ea99d636c7d6f00b849e40d
krishnalavanya <krishnalavanyakasireddi@gmail.com>
2026-01-29 18:12:50 -0500

    variables

--- 08-installation.sh
+++ 08-installation.sh
@@ -3,8 +3,16 @@ userid=$(id -u)
 
 if [ $userid -ne 0 ]; then
     echo "please run the script with root user id"
-    
+    exit 1
 fi
 
-echo "installing nginx"
-dnf install Nginx -y
+echo "installing java"
+dnf install java -y
+
+if [ $? -ne 0 ]; then
+
+   echo "installing java failure"
+   exit 1
+else
+   echo "installing java is success"
+fi
