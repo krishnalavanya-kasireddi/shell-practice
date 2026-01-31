960a3f2736a88c496cbc09852fae27a37134a336
krishnalavanya <krishnalavanyakasireddi@gmail.com>
2026-01-31 15:09:29 -0500

    variables

---
+++ functions.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo "please run the script with root user id"
+    exit 1
+fi
+
+validate(){
+    if [ $1 -ne 0 ]; then
+
+   echo "$2... failure"
+   exit 1
+else
+   echo "$2.... success"
+fi
+}
+
+dnf install java -y
+validate $? "installing java"
+
+dnf install python -y
+validate $? "installing python"
+
+dnf install mysql -y
+validate $? "installing mysql"
+
