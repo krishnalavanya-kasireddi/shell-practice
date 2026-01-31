60c8d7c6168108e322878c3c56e1d1b748140512
krishnalavanya <krishnalavanyakasireddi@gmail.com>
2026-01-31 16:00:11 -0500

    variables

--- functions.sh
+++ functions.sh
@@ -22,16 +22,7 @@ validate $? "installing java"
 dnf install python -y
 validate $? "installing python"
 
-dnf install https://dev.mysql.com/get/mysql80-community-release-el9-1.noarch.rpm -y
-validate $? "installing mysql repo"
-
-rpm --import https://repo.mysql.com/RPM-GPG-KEY-mysql-2022
-validate $? "importing mysql gpg key"
-
-dnf clean all
-dnf makecache
-
-dnf install mysql-community-server -y
+dnf install mysql -y
 validate $? "installing mysql"
 
 
